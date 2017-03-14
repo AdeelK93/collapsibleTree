9ce95535597a08d780c82f5b6208a0ef8f041035
Adeel Khan <akhan@molecularrebar.com>
2017-03-14 14:49:23 -0500

    IE11 support

--- inst/htmlwidgets/collapsibleTree.js
+++ inst/htmlwidgets/collapsibleTree.js
@@ -148,10 +148,10 @@ HTMLWidgets.widget({
       // Creates a curved (diagonal) path from parent to the child nodes
       function diagonal(s, d) {
 
-        path = `M ${s.y} ${s.x}
-        C ${(s.y + d.y) / 2} ${s.x},
-        ${(s.y + d.y) / 2} ${d.x},
-        ${d.y} ${d.x}`
+        path = "M " + s.y + " " + s.x +
+        " C " + (s.y + d.y) / 2 + " " +
+        s.x + ", " + (s.y + d.y) / 2 + " " + d.x + ", " +
+        d.y + " " + d.x;
 
         return path
       }
