2f6c7e78752de2fc909cfc34b453a60cac2a53dc
steadyfish <dhrumins68@gmail.com>
2017-05-20 23:22:23 -0400

    updated collapsibleTree function
    Details:
    - Added a collapsibleTree generic
    - Existing collapsibleTree function is now coallapsibleTree.data.frame
    - Added a new function collapsibleTree.Node to directly operate on data.tree (Node) objects
    
    To do:
    - Remove unnecessary code (currently commented out)
    - Add examples
    - Update function/parameters documentation

--- R/collapsibleTree.R
+++ R/collapsibleTree.R
@@ -3,7 +3,8 @@
 #' Interactive Reingold-Tilford tree diagram created using D3.js,
 #' where every node can be expanded and collapsed by clicking on it.
 #'
-#' @param df a data frame from which to construct a nested list
+#' @param \code{...} a \code{data.frame} from which to construct a nested list or
+#'  a preconstructed \code{data.tree}
 #' @param hierarchy a character vector of column names that define the order
 #' and hierarchy of the tree network
 #' @param root label for the root node
@@ -59,11 +60,23 @@
 #' @importFrom data.tree Aggregate
 #' @importFrom stats complete.cases
 #' @export
-collapsibleTree <- function(df, hierarchy, root = deparse(substitute(df)),
-                  inputId = NULL, width = NULL, height = NULL,
-                  attribute = "leafCount", aggFun = sum,
-                  fill = "lightsteelblue", fillByLevel = TRUE,
-                  linkLength = NULL, fontSize = 10, tooltip = FALSE) {
+collapsibleTree <- function(..., hierarchy, root = deparse(substitute(df)),
+                            inputId = NULL, width = NULL, height = NULL,
+                            attribute = "leafCount", aggFun = sum,
+                            fill = "lightsteelblue", fillByLevel = TRUE,
+                            linkLength = NULL, fontSize = 10, tooltip = FALSE){
+  UseMethod("collapsibleTree")
+}
+
+#' Create Interactive Collapsible Tree Diagrams from a \code{data.frame}
+#'
+#' @param df a data frame from which to construct a nested list
+#' @export
+collapsibleTree.data.frame <- function(df, hierarchy, root = deparse(substitute(df)),
+                                       inputId = NULL, width = NULL, height = NULL,
+                                       attribute = "leafCount", aggFun = sum,
+                                       fill = "lightsteelblue", fillByLevel = TRUE,
+                                       linkLength = NULL, fontSize = 10, tooltip = FALSE) {
 
   # preserve this name before evaluating df
   root <- root
@@ -142,6 +155,118 @@ collapsibleTree <- function(df, hierarchy, root = deparse(substitute(df)),
 
   # keep only the fill attribute in the final JSON
   data <- data.tree::ToListExplicit(node, unname = TRUE, keepOnly = jsonFields)
+
+  # pass the data and options using 'x'
+  x <- list(
+    data = data,
+    options = options
+  )
+
+  # create the widget
+  htmlwidgets::createWidget(
+    "collapsibleTree", x, width = width, height = height,
+    htmlwidgets::sizingPolicy(viewer.padding = 0)
+  )
+}
+
+
+
+#' Create Interactive Collapsible Tree Diagrams from a \code{data.tree}
+#'
+#' @param df a data tree from which to a collapsible tree diagram
+#' @export
+collapsibleTree.Node <- function(df, hierarchy_attribute = "Group",
+                                      root = df$name, inputId = NULL, width = NULL, height = NULL,
+                                      attribute = "leafCount", aggFun = sum,
+                                      fill = "lightsteelblue", fillByLevel = TRUE,
+                                      linkLength = NULL, fontSize = 10, tooltip = FALSE) {
+
+  # preserve this name before evaluating df
+  root <- root
+
+  # Deriving hierarchy variable from data.tree input
+  hierarchy = unique(ToDataFrameTree(df, hierarchy_attribute)[[hierarchy_attribute]])
+
+  # reject bad inputs
+  if(!is(df) %in% "Node") stop("df must be a data tree object")
+  # check not required for data.tree object input
+  # if(!is.character(hierarchy)) stop("hierarchy must be a character vector")
+  if(!is.character(fill)) stop("fill must be a character vector")
+  if(length(hierarchy) <= 1) stop("hierarchy vector must be greater than length 1")
+  # check not required for data.tree object input
+  # if(!all(hierarchy %in% colnames(df))) stop("hierarchy column names are incorrect")
+  # check not required for data.tree object input
+  # if(!(attribute %in% c(colnames(df), "leafCount"))) stop("attribute column name is incorrect")
+  if(attribute != "leafCount") {
+    if(any(is.na(ToDataFrameTree(attribute, "leafCount")[attribute]))) stop("attribute must not have NAs")
+  }
+
+  # check not required for data.tree object input
+  # if df has NAs, coerce them into character columns and replace them with ""
+  # if(sum(complete.cases(df[hierarchy])) != nrow(df)) {
+  #   df[hierarchy] <- lapply(df[hierarchy], as.character)
+  #   df[is.na(df)] <- ""
+  # }
+
+  # calculate the right and left margins in pixels
+  leftMargin <- nchar(root)
+  allNodes = df$Get("level")
+  rightNodes = allNodes[which(allNodes == max(allNodes))]
+  rightMargin <- max(nchar(names(rightNodes)))
+
+  # create a list that contains the options
+  options <- list(
+    hierarchy = hierarchy,
+    input = inputId,
+    attribute = attribute,
+    linkLength = linkLength,
+    fontSize = fontSize,
+    tooltip = tooltip,
+    margin = list(
+      top = 20,
+      bottom = 20,
+      left = (leftMargin * fontSize/2) + 25,
+      right = (rightMargin * fontSize/2) + 25
+    )
+  )
+
+  # not required for data.tree input
+  # the hierarchy that will be used to create the tree
+  # df$pathString <- paste(
+  #   root,
+  #   apply(df[,hierarchy], 1, paste, collapse = "//"),
+  #   sep="//"
+  # )
+
+  # convert the data frame into a data.tree node
+  # node <- data.tree::as.Node(df, pathDelimiter = "//")
+
+  # fill in the node colors, traversing down the tree
+  if(length(fill)>1) {
+    if(length(fill) != df$totalCount) {
+      stop(paste("Expected fill vector of length", df$totalCount, "but got", length(fill)))
+    }
+    df$Set(fill = fill, traversal = ifelse(fillByLevel, "level", "pre-order"))
+  } else {
+    options$fill <- fill
+  }
+
+  # only necessary to perform these calculations if there is a tooltip
+  if(tooltip) {
+    # traverse down the tree and compute the weights of each node for the tooltip
+    t <- data.tree::Traverse(df, "pre-order")
+    data.tree::Do(t, function(x) {
+      x$WeightOfNode <- data.tree::Aggregate(x, attribute, aggFun)
+      # make the tooltips look nice
+      x$WeightOfNode <- prettyNum(
+        x$WeightOfNode, big.mark = ",", digits = 3, scientific = FALSE
+      )
+    })
+    jsonFields <- c("fill", "WeightOfNode")
+  } else jsonFields <- "fill"
+
+  # keep only the fill attribute in the final JSON
+  data <- data.tree::ToListExplicit(df, unname = TRUE, keepOnly = jsonFields)
 
   # pass the data and options using 'x'
   x <- list(
